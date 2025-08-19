584a97b00b9ba65e7f27590091fcd07668e4bb37
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-19 14:27:40 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -6,26 +6,26 @@ import sectorHeadRoutes from './routes/sectorHead.js';
 import citizenRoutes from './routes/citizenRoutes.js';
 import issueRoutes from './routes/issueRoutes.js';
 import feedbackRoutes from './routes/feedbackRoutes.js';
-import adminRoutes from '../Backend/routes/adminRoutes.js';
+import adminRoutes from './routes/adminRoutes.js'; 
 import cors from 'cors';
-
 
 dotenv.config();
 connectDB();
+
 const app = express();
 app.use(express.json());
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", 
   credentials: true
 }));
+
+// routes
 app.use('/api/auth', authRoutes);
 app.use('/api/sector-head', sectorHeadRoutes);
 app.use('/api/citizen', citizenRoutes);
 app.use('/api/issues', issueRoutes);
 app.use('/api/feedback', feedbackRoutes);
-app.use('/api/issues', adminRoutes);
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use('/api/admin', adminRoutes);
+
 export default app;
