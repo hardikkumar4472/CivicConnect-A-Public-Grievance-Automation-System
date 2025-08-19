2d0daaabb4344514461aa97ab1cf6e9db84a4e86
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-19 15:52:54 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-const API_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 function Home() {
   const [selectedRole, setSelectedRole] = useState("citizen");
   const [loading, setLoading] = useState(true);
