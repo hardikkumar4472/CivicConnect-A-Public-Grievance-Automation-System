e3b96126574448d503fb4b6aab2bdfdfb69225e1
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-20 00:21:32 +0530

    Update adminRoutes.js

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -5,10 +5,10 @@ import { getDashboardSummary } from '../controllers/adminController.js';
 import { exportAllIssues } from '../controllers/adminController.js';
 import { getAdminProfile } from '../controllers/adminController.js';
 const router = express.Router();
-router.post('/broadcast', protect, sendBroadcastEmail);
-router.get('/dashboard-summary', protect, getDashboardSummary);
-router.get('/export-issues', protect, exportAllIssues);
-router.get("/profile", protect, getAdminProfile);
+router.post('https://civicconnect-1f9p.onrender.com/broadcast', protect, sendBroadcastEmail);
+router.get('https://civicconnect-1f9p.onrender.com/dashboard-summary', protect, getDashboardSummary);
+router.get('https://civicconnect-1f9p.onrender.com/export-issues', protect, exportAllIssues);
+router.get("https://civicconnect-1f9p.onrender.com//profile", protect, getAdminProfile);
 export default router;
 
 
