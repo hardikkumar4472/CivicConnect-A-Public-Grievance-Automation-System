26329d95a81835e9e27e7c862bd06f8e61bbd30a
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-19 15:58:39 +0530

    Update citizenDashboard.jsx

--- Frontend/src/pages/citizenDashboard.jsx
+++ Frontend/src/pages/citizenDashboard.jsx
@@ -6,7 +6,7 @@ import IssueCard from "./IssueCard";
 import IssueDetailsModal from "./CitizenDashboard/IssueDetailsModal";
 import NewIssueModal from "./CitizenDashboard/NewIssueModal";
 import FeedbackModal from "./FeedbackModal";
-
+const API_URL = import.meta.env.VITE_API_URL;
 export default function CitizenDashboard() {
   const [issues, setIssues] = useState([]);
   const [filteredIssues, setFilteredIssues] = useState([]);
@@ -35,7 +35,7 @@ export default function CitizenDashboard() {
     if (closedIssues.length > 0) {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/feedback/batch",
+          "${API_URL}/api/feedback/batch",
           {
             headers: { Authorization: `Bearer ${token}` },
             params: {
@@ -69,10 +69,10 @@ export default function CitizenDashboard() {
       }
 
       const [citizenRes, issuesRes] = await Promise.all([
-        axios.get("http://localhost:5000/api/citizen/me", {
+        axios.get("${API_URL}/api/citizen/me", {
           headers: { Authorization: `Bearer ${token}` }
         }),
-        axios.get("http://localhost:5000/api/issues/my", {
+        axios.get("${API_URL}/api/issues/my", {
           headers: { Authorization: `Bearer ${token}` }
         })
       ]);
@@ -124,7 +124,7 @@ export default function CitizenDashboard() {
     try {
       const token = localStorage.getItem("token");
       const response = await axios.post(
-        "http://localhost:5000/api/issues/report",
+        "${API_URL}/api/issues/report",
         newIssueData,
         { headers: { Authorization: `Bearer ${token}` } }
       );
@@ -142,7 +142,7 @@ export default function CitizenDashboard() {
       setExporting(true);
       const token = localStorage.getItem("token");
       const response = await axios.get(
-        "http://localhost:5000/api/issues/export-issues",
+        "${API_URL}/api/issues/export-issues",
         {
           headers: { Authorization: `Bearer ${token}` },
           responseType: "blob"
@@ -181,7 +181,7 @@ export default function CitizenDashboard() {
   try {
      const token = localStorage.getItem("token");
     await axios.post(
-      "http://localhost:5000/api/feedback/submit",
+      "${API_URL}/api/feedback/submit",
       { issueId, rating, comment },
       { headers: { Authorization: `Bearer ${token}` } }
     );
