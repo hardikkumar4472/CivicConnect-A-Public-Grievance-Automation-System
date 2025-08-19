5db7f17f4ff011042706f7b05dde10604cd4d2b7
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-08-20 00:22:39 +0530

    Update feedbackRoutes.js

--- Backend/routes/feedbackRoutes.js
+++ Backend/routes/feedbackRoutes.js
@@ -4,6 +4,6 @@ import authCitizen from '../middleware/authCitizen.js';
 import { protect } from '../middleware/auth.js';
 import { getSectorWiseRatings } from '../controllers/feedbackController.js';
 const router = express.Router();
-router.post('/submit', authCitizen, submitFeedback);
-router.get("/sector-ratings",  protect ,getSectorWiseRatings);
+router.post('https://civicconnect-1f9p.onrender.com/submit', authCitizen, submitFeedback);
+router.get("https://civicconnect-1f9p.onrender.com/sector-ratings",  protect ,getSectorWiseRatings);
 export default router;
